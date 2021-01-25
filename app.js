6acbde4db847a5d2bf9b699222ae22f7b2d34bba
jaykatkani <68242845+jaykatkani@users.noreply.github.com>
2021-01-25 12:40:24 +0530

    hostel page

--- app.js
+++ app.js
@@ -5,18 +5,19 @@ const methodOverride = require("method-override");
 const passport = require("passport");
 const session = require("express-session");
 const MongoStore = require("connect-mongo")(session);
-const url = "mongodb://localhost/HAB_DB";
+const url = "mongodb+srv://jayk:jk1628@2018@cluster0.dhg1e.mongodb.net/test?retryWrites=true&w=majority";
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 require("./config/passport")(passport);
 
 mongoose.connect(url, {
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+
+}).then(() => console.log("Successful DB connection")).catch((err) => console.error("DB connection fail"));
 
 const userRoutes = require("./routes/user.routes");
 const adminRoutes = require("./routes/admin.routes");
@@ -28,18 +29,18 @@ app.use(express.static(__dirname + "/uploads"));
 app.use(methodOverride("_method"));
 
 app.use(
-  session({
-    secret: "Once again rusty is the cutest dog",
-    resave: false,
-    saveUninitialized: false,
-  })
+    session({
+        secret: "Once again rusty is the cutest dog",
+        resave: false,
+        saveUninitialized: false,
+    })
 );
 
 app.use(passport.initialize());
 app.use(passport.session());
 app.use((req, res, next) => {
-  res.locals.currentUser = req.user;
-  next();
+    res.locals.currentUser = req.user;
+    next();
 });
 
 app.set("view engine", "ejs");
@@ -48,5 +49,5 @@ app.use("/", userRoutes);
 app.use("/", adminRoutes);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+    console.log(`Server is running on port ${PORT}.`);
 });
