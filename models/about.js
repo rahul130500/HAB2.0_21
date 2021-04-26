29256cd3cedf1a294cdd5f150588dd0a2d76a9e8
Pranjal Chourasia <61688724+pranjal5399@users.noreply.github.com>
2021-04-26 10:47:23 +0530

    home description bug fixed

--- models/about.js
+++ models/about.js
@@ -1,11 +1,10 @@
 const mongoose = require("mongoose");
 
 const AboutSchema = mongoose.Schema({
-  title: { type: String, required: true },
+  title: { type: String },
   description: { type: String, required: true },
-  
-  priority_number: { type: Number, required: true },
 
+  priority_number: { type: Number, required: true },
 });
 
 module.exports = mongoose.model("About", AboutSchema);
