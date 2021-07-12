52a7a954332ddd97b8981570e6bbb4e8afc5b742
Rahul Aggarwal <rahulaggarwal13500@gmail.com>
2021-07-12 18:31:13 +0530

    user model updated

--- models/user.js
+++ models/user.js
@@ -1,14 +1,13 @@
 const mongoose = require("mongoose");
-const passportLocalMongoose = require("passport-local-mongoose");
 
 const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true },
-  password: { type: String },
+  email: { type: String, required: true },
+  accessToken: { type: String, required: true, select: false },
   name: { type: String },
   isAdmin: { type: Boolean, default: false },
+  isHostelAdmin: { type: Boolean, default: false },
+  hostel: { type: String },
   role: { type: String, required: true, default: "NA" },
   contact: { type: Number, length: 10 },
 });
-
-UserSchema.plugin(passportLocalMongoose);
 module.exports = mongoose.model("User", UserSchema);
